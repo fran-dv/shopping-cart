f28c2e1c1566bee9dbc6769cb6455833a9a62226
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-11 12:20:40 -0300

    feat: add a basic hero to the home with call to action

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,33 @@
+import { Button } from "@/components";
+import styles from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "@/routes";
+
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(Paths.SHOP);
+  };
+
   return (
     <>
-      <h1>Here is the home!</h1>
+      <div className={styles.hero}>
+        <h1>Welcome to the mock store!</h1>
+        <h3>
+          This is actually only a mock shopping cart made for practice purposes
+        </h3>
+        <p>
+          Browse our amazing selection of imaginary products. No wallet
+          required—just your imagination!
+        </p>
+        <Button
+          onClick={handleClick}
+          content="Explore products"
+          cta={true}
+          className={styles.cta}
+        />
+      </div>
     </>
   );
 };
