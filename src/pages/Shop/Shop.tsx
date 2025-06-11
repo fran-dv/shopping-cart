60fc1163a870a5098524260176bc0504d24719b8
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-11 17:38:58 -0300

    feat: implement the products views in the shop page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,3 +1,47 @@
+import { useProducts } from "@/hooks";
+import styles from "./Shop.module.css";
+import { ProductCard } from "@/components";
+
 export const Shop = () => {
-  return <h1>Here is the shop! what are you going to buy?</h1>;
+  const { loading, error, products } = useProducts();
+
+  const handleAddToCart = () => {
+    console.log("Added to cart!");
+  };
+
+  console.log(products);
+  return (
+    <div className={styles.shopContainer}>
+      {loading && (
+        <div className={styles.loading}>
+          <h2>Loading products...</h2>
+        </div>
+      )}
+      {error && (
+        <div className={styles.error}>
+          <h2>There was an error fetching the products: {error.message}</h2>
+        </div>
+      )}
+      {!loading && products && (
+        <>
+          <div className={styles.header}>
+            <h1>Our mock products</h1>
+          </div>
+
+          <div className={styles.productsContainer}>
+            {products.map((p) => (
+              <ProductCard
+                key={p.id}
+                title={p.title}
+                description={p.description}
+                imageSrc={p.image}
+                onClickAddToCart={handleAddToCart}
+                price={p.price}
+              />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
