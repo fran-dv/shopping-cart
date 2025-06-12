97666e58013313ca0313ce6081a744c85fbbe49c
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-12 13:19:41 -0300

    feat: implement cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,82 @@
+import { useCartStore } from "@/stores";
+import styles from "./Cart.module.css";
+import { Button, CartItem } from "@/components";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "@/routes";
+
 export const Cart = () => {
+  const { items, incrementItemAmount, decrementItemAmount, removeItem } =
+    useCartStore();
+  const navigate = useNavigate();
+
+  const subtotalPrice = items.reduce(
+    (subtotal, it) => subtotal + it.product.price * it.amount,
+    0,
+  );
+  const deliveryCost = 20;
+  const totalPrice = subtotalPrice + deliveryCost;
+
+  const handleCheckout = () => {};
+
   return (
-    <>
-      <h1>Here is the cart! ready to checkout?</h1>
-    </>
+    <div className={styles.container}>
+      {items.length === 0 && (
+        <div className={styles.emptyView}>
+          <h3>You don't have any products in your cart yet. Add some!</h3>
+          <Button
+            content="Go to products!"
+            cta={true}
+            onClick={() => navigate(Paths.SHOP)}
+          />
+        </div>
+      )}
+      {items.length > 0 && (
+        <>
+          <h2 className={styles.title}>Your cart</h2>
+          <div className={styles.contentContainer}>
+            <div className={styles.itemsContainer}>
+              {items.map((it) => (
+                <CartItem
+                  key={it.id}
+                  title={it.product.title}
+                  price={it.product.price}
+                  amount={it.amount}
+                  id={it.id}
+                  imageSrc={it.product.image}
+                  onIncrement={(itemId: number) => incrementItemAmount(itemId)}
+                  onDecrement={(itemId: number) => decrementItemAmount(itemId)}
+                  onDelete={(itemId: number) => removeItem(itemId)}
+                />
+              ))}
+            </div>
+
+            <div className={styles.orderInfo}>
+              <div className={styles.section}>
+                <h4>Subtotal: </h4>
+                <p>${subtotalPrice.toFixed(2)}</p>
+              </div>
+
+              <div className={styles.section}>
+                <h4>Delivery: </h4>
+                <p>${deliveryCost.toFixed(2)}</p>
+              </div>
+
+              <div className={`${styles.section} ${styles.total}`}>
+                <h4>Total: </h4>
+                <p>${totalPrice.toFixed(2)}</p>
+              </div>
+
+              <div className={`${styles.section} ${styles.checkout}`}>
+                <Button
+                  content="Checkout"
+                  onClick={handleCheckout}
+                  className={styles.button}
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
