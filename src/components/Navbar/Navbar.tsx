78d7c4dc5bf12f9b06c48f94bf6ba55c0675f261
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-11 22:48:29 -0300

    feat: add badge in the cart icon with the amount of items in the cart

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,12 @@ import { Paths } from "@/routes";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { ShoppingBag } from "lucide-react";
+import { Badge } from "@/components";
+import { useCartStore } from "@/stores";
 
 export const Navbar = () => {
+  const { items } = useCartStore();
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -17,7 +21,13 @@ export const Navbar = () => {
           <p>SHOP</p>
         </Link>
         <Link to={Paths.CART} className={styles.link} title="Go to cart">
-          <ShoppingBag className={styles.navIcon} />
+          {items.length > 0 ? (
+            <Badge content={items.length}>
+              <ShoppingBag className={styles.navIcon} />
+            </Badge>
+          ) : (
+            <ShoppingBag className={styles.navIcon} />
+          )}
         </Link>
       </div>
     </div>
